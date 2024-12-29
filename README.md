f105567a0d60a11dafa4cee4b2fc3f10e542065e
zuoyonghui <zzhattzzh@126.com>
2024-12-30 01:01:38 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# kittytabs.nvim
+
+在 Neovim 中快速打开 Kitty 终端标签页的插件。
+
+## 安装
+
+使用 [lazy.nvim](https://github.com/folke/lazy.nvim): 
+
+## 目录结构 
---
+++ plugin/kittytabs.lua
@@ -0,0 +1,9 @@
+if vim.g.loaded_kittytabs == 1 then
+    return
+end
+vim.g.loaded_kittytabs = 1
+
+local kittytabs = require('kittytabs')
+
+-- 创建命令
+vim.api.nvim_create_user_command("KittyStartupTabs", kittytabs.kittyStartupTabs, {}) 
